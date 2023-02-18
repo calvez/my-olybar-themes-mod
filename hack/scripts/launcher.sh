4633fd2b0fdf74a6140346ebfa8fab57537abccf
calvez <nemeth.lorant@gmail.com>
2023-02-18 22:44:25 +0100

    hackl is almost network is bad

--- hack/scripts/launcher.sh
+++ hack/scripts/launcher.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-rofi -no-config -no-lazy-grab -show drun -modi drun -theme ~/.config/polybar/hack/scripts/rofi/launcher.rasi
+# rofi -no-config -no-lazy-grab -show drun -modi drun -theme ~/.config/polybar/hack/scripts/rofi/launcher.rasi
+rofi -show drun -theme launchers/type-7/style-4
